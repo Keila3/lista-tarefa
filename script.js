25acf9e723e438d2b823b3305788c9beb2840bdb
keila <kbkeilabarbosa@gmail.com>
2026-04-25 01:53:54 +0000

    adiciona funcionalidade para armazenar tarefas no localStorage

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-
+let tarefas = [];
 const botao = document.getElementById('btn');
 const tarefa = document.getElementById('tarefa');
 const lista = document.getElementById('lista'); 
-botao.addEventListener('click', function(){ 
+botao.addEventListener('click', function(){
  const valor = tarefa.value;
+ tarefas.push(valor);
  const item = document.createElement('li');
  item.textContent = valor;
  lista.appendChild(item);
+ localStorage.setItem('tarefas', JSON.stringify(tarefas));
  tarefa.value = '';
 } )
